ab854f961aa3089331f3f247f426fec308d1b34e
Jakub <jakubnowakj@gmail.com>
2025-07-14 15:45:44 +0200

    edit main page

--- TaskFix/urls.py
+++ TaskFix/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from TaskFix.views import home_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('company/', include('company.urls')),
     path('', home_view, name='home'),
     #path('', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
